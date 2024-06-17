91f7344a684d2918d87c142f30663f4c5fe5a9ec
Gourav Yadav <56175133+v0idgy@users.noreply.github.com>
2024-06-17 23:17:50 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -73,6 +73,7 @@ def list_accounts():
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
 
+    
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 # ... place you code here to LIST accounts ...
